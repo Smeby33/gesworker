f73cd6850bbc72d03f1779c7abaeb8cd3ff49b35
Emmanuel (Edoh) SAGOUNAMA-MENSAH <smebedoh33@gmail.com>
2025-05-06 19:59:36 +0100

    Update api.js

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Configuration de base
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
-  timeout: 10000, // 10 secondes timeout
+  timeout: 60000, // 10 secondes timeout
   headers: {
     'Content-Type': 'application/json',
   }
